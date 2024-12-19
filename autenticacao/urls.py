50d932267bb2472c191959db51f22221c9722aba
Daiana <daianagarcia2805@gmail.com>
2024-12-19 13:20:40 -0300

    update

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = 'autenticacao'
-urlpattherns = [
-    path('',views.login,name='login'),
+
+urlpatterns = [
+    path('', views.login, name='login'),
+    path("get_perfis/", views.get_perfis, name="get_perfis"),
+    path("logout/", views.logout, name="logout"),
 ]
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('autenticacao.urls')),
+    path('core/', include('core.urls')),
 ]
- 
